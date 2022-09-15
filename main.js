31bebf4e398d888b43da67ea974e5b37e2bf3699
Ian Almeida <iangtaonline1@gmail.com>
2022-09-15 19:21:40 -0300

    modificando a function para tocar o som de qualquer elemento

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementAudio){
+    document.querySelector(idElementAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
 while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = tocaSomPom;
+    listaDeTeclas[contador].onclick = tocaSom;
     contador = contador + 1;
     console.log(contador);
 }
