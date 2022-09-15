2942527e4224b6c6a9f3019254ee802e6c70fb7f
Ian Almeida <iangtaonline1@gmail.com>
2022-09-15 10:07:37 -0300

    criando algumas funções que são ativadas onclick

--- main.js
+++ main.js
@@ -3,6 +3,18 @@ function tocaSomPom(){
 }
 
 function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play()
+    document.querySelector('#som_tecla_clap').play();
 }
 
+function tocaSomSplash(){
+    document.querySelector('#som_tecla_splash').play();
+}
+
+function tocaSomToim(){
+    document.querySelector('#som_tecla_toim').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+document.querySelector('.tecla_toim').onclick = tocaSomToim;
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
