6ecd0236f392a1e41781bb75c100ffe541c1986d
Ian Almeida <iangtaonline1@gmail.com>
2022-09-15 17:06:44 -0300

    criando estrutura de repetição

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
@@ -14,7 +15,25 @@ function tocaSomToim(){
     document.querySelector('#som_tecla_toim').play();
 }
 
+function tocaSomTic(){
+    document.querySelector('#som_tecla_tic').play();
+}
+
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
+document.querySelector('.tecla_tic').onclick = tocaSomTic;
 document.querySelector('.tecla_clap').onclick = tocaSomClap;
 document.querySelector('.tecla_toim').onclick = tocaSomToim;
 document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+*/
+
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+let contador = 0
+
+while(contador < 9 ){
+    listaDeTeclas[0].onclick = tocaSomPom;
+    contador = contador++
+}
