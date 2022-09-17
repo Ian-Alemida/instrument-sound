b11ec1dde85979e876058f407603335bab73b7d4
Ian Almeida <iangtaonline1@gmail.com>
2022-09-17 16:49:13 -0300

    criando condicional

--- main.js
+++ main.js
@@ -12,12 +12,27 @@ for (let i = 0; i < listaDeTeclas.length; i++) {
     const tecla = listaDeTeclas[i]
         //a const 'instrumento' guarda o valor da segunda class da const 'tecla'
     const instrumento = tecla.classList[1]
-        //uma string q guarda o id do instrumento referente ao botão q esta sendo pressionado
-    const idAudio = `#som_${instrumento}`
+    //uma string q guarda o id do instrumento referente ao botão q esta sendo pressionado
+    const idAudio = `#som_${instrumento}`//Template string
 
     //Ao clicar em algum botão ativara uma function q vai reproduzir o som equivalente a essa tecla
     tecla.onclick = function(){
         tocaSom(idAudio)
+    };
+
+    //Adicoinando class de um elemento 
+    tecla.onkeydown = function(evento){
+        console.log(evento.code)
+
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+
+        }
+    };
+
+    //Removendo class de um elemento 
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
     };
 }
 // O codigo pega o botão q esta sendo precionado atraves de seu indice na listaDeTeclas, logo em seguida ele pega sua class para completar a string do idAudio e ativa o som desejado pelo usuario com a function tocaSom 
